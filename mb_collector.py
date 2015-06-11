68c466b19cc7776d2638b61fc33b0110d9552238
city-pulse <tolmach@me.com>
2015-06-11 18:04:43 +0300

    Datetime fix

--- mb_collector.py
+++ mb_collector.py
@@ -54,13 +54,14 @@ class TwitterStreamThread(threading.Thread):
 				stream = tw_api.request('statuses/filter', {'locations':'37.364307,55.558649,37.831226,55.918149'})
 				for item in stream:
 					if 'coordinates' in item and item['coordinates']:
+						print item['created_at']
 						print datetime.datetime.now().isoformat(), item['coordinates']['coordinates'], item['text']
 						q = 'INSERT INTO tweets(id, text, lat, lng, tstamp) VALUES ({}, "{}", {}, {}, "{}");'.format(
 							item['id_str'], 
 							MySQLdb.escape_string(item['text'].encode('utf-8', 'replace')),
 							item['coordinates']['coordinates'][1],
 							item['coordinates']['coordinates'][0],
-							datetime.datetime(item['created_at'][4:], '%b %d %H:%M:%S +0000 %Y')
+							datetime.datetime.strptime(item['created_at'][4:], '%b %d %H:%M:%S +0000 %Y')
 							)
 						exec_mysql(q)
 						get_media(item['entities'], item['id_str'])
