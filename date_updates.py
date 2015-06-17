4a892afb3b175a82d8c7187082b3f6788bf64318
city-pulse <tolmach@me.com>
2015-06-17 14:03:30 +0300

    Missing values in updaters fix #2

--- date_updates.py
+++ date_updates.py
@@ -25,7 +25,7 @@ query.Query('SET character_set_connection=utf8mb4;')
 while True:
 	query.Query('SELECT * FROM tweets WHERE ISNULL(tstamp) LIMIT 50;')
 	tweet_ids = query.record
-	if len(tweets_ids) == 0:
+	if len(tweet_ids) == 0:
 		break
 	ids = ','.join([str(x['id']) for x in tweet_ids])
 	r = tw_api.request('statuses/lookup', {'id':ids})
@@ -33,7 +33,7 @@ while True:
 		print item['id'], item['created_at']
 		query.Query('UPDATE tweets SET tstamp="{}" WHERE id={}'.format(datetime.datetime.strptime(item['created_at'][4:], '%b %d %H:%M:%S +0000 %Y'), item['id']))
 		tweet_ids.pop(tweet_ids.index(item['id']))
-	for tid in tweets_ids:
+	for tid in tweet_ids:
 		print tid, 'Not found'
 		query.Query('UPDATE tweets SET tstamp="2000-01-01 00:00:00" WHERE id={}'.format(tid))
 	sleep(15)
