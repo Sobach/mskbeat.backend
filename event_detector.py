280d103095059cc98497a3b6bfc15c21c10e0982
city-pulse <tolmach@me.com>
2015-08-28 18:02:14 +0300

    Event detector scetch: kNN, DBSCAN

--- event_detector.py
+++ event_detector.py
@@ -14,12 +14,17 @@ import datetime
 
 # NEW
 import math
+import itertools
+from sklearn.neighbors import BallTree
+from sklearn.cluster import DBSCAN
 
 # CONSTANTS
 from settings import *
 
 # MATH
 import numpy as np
+
+import pprint
 
 redis_db = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
 PySQLPool.getNewPool().maxActiveConnections = 1
@@ -39,16 +44,47 @@ class ThresholdUpdaterThread(threading.Thread):
 			time.sleep((wakeup - datetime.datetime.now()).seconds)
 
 class GotMsg(threading.Thread):
+	def __init__(self):
+		super(GotMsg, self).__init__()
+		self.bbox = [float(x) for x in TW_LOCATIONS.split(',')]
+
 	def run(self):
-		bbox = [float(x) for x in TW_LOCATIONS.split(',')]
 		while True:
 			data = json.loads(redis_db.blpop('upd_queue')[1])
-			x,y,n = online_cube_coords(data, bbox)
-			q = 'UPDATE threshold SET reality = reality + 1 WHERE lng = {} AND lat = {} AND net = {};'.format(x, y, n)
-			exec_mysql(q)
-			current_level = exec_mysql('SELECT expect, reality FROM threshold WHERE lng = {} AND lat = {} AND net = {};'.format(x, y, n))[0][0]
+			x,y,n = online_cube_coords(data, self.bbox)
+			exec_mysql('''UPDATE threshold SET reality = reality + 1 WHERE lng = {} AND lat = {} AND net = {};'''.format(x, y, n))
+			current_level = exec_mysql('''SELECT expect, reality FROM threshold WHERE lng = {} AND lat = {} AND net = {};'''.format(x, y, n))[0][0]
 			if current_level['reality'] > current_level['expect']:
-				redis_db.rpush('event_grain', json.dumps({'lng':x, 'lat':y, 'net':n, 'hour':int(math.floor(time.time()/3600))}))
+				exec_mysql('''INSERT INTO event_grains(net,lng,lat,hour,deviation) VALUES ({},{},{},{},{}) ON DUPLICATE KEY UPDATE deviation=VALUES(deviation);'''.format(
+					n, x, y, int(math.floor(time.time()/3600)), float(current_level['reality'])/current_level['expect']))
+
+class ClusterEvents(threading.Thread):
+	def __init__(self, thres_nnd=1.8, thres_deviation=3):
+		super(ClusterEvents, self).__init__()
+		self.thresholds = (thres_nnd, thres_deviation)
+		
+	def run(self):
+		while True:
+			data = exec_mysql('SELECT * FROM event_grains;')[0]
+			tree_data = [(x['net'], x['lng'], x['lat'], x['hour']) for x in data]
+			tree = BallTree(np.array(tree_data))
+			distances = tree.query(tree_data, k=2, return_distance=True)[0][:,1]
+			filtered_tree = []
+			for i in range(len(data)):
+				if data[i]['deviation'] >= self.thresholds[1] or distances[i] <= self.thresholds[0]:
+					filtered_tree.append([data[i]['net'], data[i]['lat'], data[i]['lng'], data[i]['hour']])
+					#filtered_tree[-1]['nnd'] = distances[i]
+			cluster = DBSCAN(eps=self.thresholds[0], min_samples=1)
+			clusters = cluster.fit_predict(np.array(filtered_tree))
+			data = {(x['net'], x['lat'], x['lng'], x['hour']):x['deviation'] for x in data}
+			event_candidates = []
+			for element in set(clusters):
+				coords = [tuple(filtered_tree[i]) for i in range(len(filtered_tree)) if clusters[i] == element]
+				weight = sum([data[x] for x in coords])
+				event_candidates.append({'coords':coords, 'weight':weight})
+			pprint.pprint(event_candidates)
+			break
+			time.sleep(60)
 
 def exec_mysql(cmd):
 	query = PySQLPool.getNewQuery(mysql_db, commitOnEnd=True)
@@ -71,7 +107,7 @@ def online_cube_coords(req_item, bbox, grain=100):
 	else: n = 2
 	return x,y,n
 
-def get_average_array(hour, min_date=(datetime.datetime.now() - datetime.timedelta(days=14)), max_date=datetime.datetime.now(), grain=100):
+def get_average_array(hour, min_date=(datetime.datetime.now() - datetime.timedelta(days=14)), max_date=datetime.datetime.now(), grain=100, min_threshold=3):
 	days = int((max_date - min_date).days) + 1
 	bbox = [float(x) for x in TW_LOCATIONS.split(',')]
 	q = 'SELECT lat, lng, tstamp, network FROM tweets WHERE HOUR(tstamp) = {} AND tstamp <= "{}" AND tstamp >= "{}" AND lng > {} AND lat > {} AND lng <  {} AND lat < {}'.format(hour, max_date, min_date, bbox[0], bbox[1], bbox[2], bbox[3])
@@ -80,16 +116,16 @@ def get_average_array(hour, min_date=(datetime.datetime.now() - datetime.timedel
 	density = np.zeros((3, days, grain, grain), dtype=np.uint16)
 	get_coords(req_item=np.array(data), density=density, bbox=bbox, min_date=min_date)
 	thresholds = (np.mean(density, axis=1) + 2 * np.std(density, axis=1)).astype(np.uint16)
+	thresholds[np.where(thresholds < min_threshold)] = min_threshold
 	return thresholds
 
-def store_array(array):
+def store_array(array, grain=100):
 	query = PySQLPool.getNewQuery(mysql_db, commitOnEnd=True)
-	for n in range(3):
-		for i in range(100):
-			for j in range(100):
-				query.Query('INSERT INTO threshold(net,lng,lat,expect,reality) VALUES ({},{},{},{},0) ON DUPLICATE KEY UPDATE expect={}, reality=0;'.format(
-					n, i, j, array[n,i,j], array[n,i,j], n, i, j))
+	query.executeMany('''INSERT INTO threshold(net,lng,lat,expect,reality) VALUES (%s,%s,%s,%s,0) ON DUPLICATE KEY UPDATE expect=VALUES(expect), reality=0;''', 
+		list((x[0], x[1], x[2], array[x[0], x[1], x[2]]) for x in itertools.product(range(3), range(grain), range(grain))))
 
-t = get_average_array(15)
-print np.sum(t)
-store_array(t)
+#maxt = datetime.datetime.strptime('2015-07-17 00:00:00', '%Y-%m-%d %H:%M:%S')
+#mint = datetime.datetime.strptime('2015-07-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+#t = get_average_array(15, mint, maxt)
+#store_array(t)
+ClusterEvents().start()
